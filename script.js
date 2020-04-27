c7e7e71360d802f94561738b7d80a7d9b26446ae
ritafulop108 <63658027+ritafulop108@users.noreply.github.com>
2020-04-27 09:24:47 -0500

    initialImigaData

---
+++ script.js
@@ -0,0 +1,59 @@
+var data1 = {
+    photo: "images/nz_01.jpeg",
+    title: "Rocks",
+    description: "Castle Hill picknic and meditation",
+};
+
+
+var data2 = {
+    photo: "images/nz_02.jpeg",
+    title: "Castle Hill",
+    description: "Black rocks",
+};
+
+
+var data3 = {
+    photo: "images/nz_03.jpeg",
+    title: "Bridge",
+    description: "Ashanee crossing the bridge",
+};
+
+
+var data4 = {
+    photo: "images/nz_04.jpeg",
+    title: "Meditation",
+    description: "Ashanee meditating at the river",
+};
+
+
+var data5 = {
+    photo: "images/nz_05.jpeg",
+    title: "Glacier rocks",
+    description: "Rocks formed by a glacier river",
+};
+
+
+var data6 = {
+    photo: "images/nz_06.jpeg",
+    title: "Glacier walk",
+    description: "Walking to the glacier with friends.",
+};
+
+
+var data7 = {
+    photo: "images/nz_07.jpeg",
+    title: "Mountain lake",
+    description: "Reflections from the mountain lake.",
+};
+
+
+var data8 = {
+    photo: "images/nz_08.jpeg",
+    title: "Road trip stop",
+    description: "Road trip stop and quick meditation at a lake.",
+};
+
+
+let currentPhoto = 0;
+let imagesData = [data1, data2, data3, data4, data5, data6, data7, data8];
+$('#photo').attr('src', imagesData[currentPhoto].photo);
